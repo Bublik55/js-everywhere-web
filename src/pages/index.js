cc05cf1ae99a64e2de041e42a983321a5ed4be60
Bublik55 <45381271+Bublik55@users.noreply.github.com>
2021-09-01 21:07:54 +0600

    SignUp form with redirect page 162/183

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Home from './home';
 import NotePage from './note';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
+import SignUp from './signup';
 
 const Pages = () => {
 	return (
@@ -16,6 +17,7 @@ const Pages = () => {
 				<Route exact path="/note/:id" component={NotePage} />
 				<Route exact path="/mynotes" component={MyNotes} />
 				<Route exact path="/favorites" component={Favorites} />
+				<Route exact path="/signup" component={SignUp} />
 			</Layout>
 		</Router>
 	);
